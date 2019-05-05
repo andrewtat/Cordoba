761c0dfafb2227ce361a023e4ca0febaee5c5169
Andrew Tat <andrewtat@me.com>
2019-05-05 12:38:20 -0600

    Remove commented Facebook Graph API code

--- src/GraphAPI.js
+++ src/GraphAPI.js
@@ -15,24 +15,6 @@ export default class GraphAPI extends Component {
         document.removeEventListener('FBObjectReady', this.initializeFacebookLogin);
     }
 
-    // getAccessToken = () => {
-    //     this.setState({
-    //         accessToken: this.exchangeToken(this.getCookieValue("access_token"))
-    //     });
-    // }
-
-    // exchangeToken = accessToken => {
-    //     const exchangeTokenURL = '/oauth/access_token?grant_type=fb_exchange_token&client_id=' + this.state.clientID + '&client_secret=' + this.state.clientSecret + '&fb_exchange_token=' + accessToken;
-    //     this.FB.api(exchangeTokenURL, response => {
-    //         return response.access_token;
-    //     });
-    // }
-
-    // getCookieValue(a) {
-    //     var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
-    //     return b ? b.pop() : '';
-    // }
-
     initializeFacebookLogin = () => {
         this.FB = window.FB;
         this.checkLoginStatus();
@@ -66,7 +48,6 @@ export default class GraphAPI extends Component {
 
     setInstagramUserData = userData => {
         const instagramUserID = userData.data[0].connected_instagram_account.id;
-        // const accessToken = userData.data[0].access_token;
         const instagramUserDataURL = '/' + instagramUserID + "?fields=name,username,profile_picture_url,followers_count,follows_count,media_count,media{id,caption,like_count,media_type,media_url,timestamp}";
         this.FB.api(instagramUserDataURL, instagramUserData => {
             const instagramUser = {
