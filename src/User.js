9384d9172c035aae96780a7665e026c26fc4f39a
Andrew Tat <andrewtat@me.com>
2019-05-07 09:50:56 -0600

    Update user information display

--- src/User.js
+++ src/User.js
@@ -17,16 +17,17 @@ export default class User extends Component {
             <div id="user">
                 <div id="bio">
                     <div id="profile-picture">
-                        <img src={this.state.user.profile_picture_url} width="400" height="400" />
+                        <img src={this.state.user.profile_picture_url} alt={this.state.user.name} width="400" height="400" />
                     </div>
-                    <h1>{this.state.user.name}</h1>
-                    <h2>@{this.state.user.username}</h2>
+                    <p>{this.state.user.name}</p>
+                    <p>@{this.state.user.username}</p>
                     <div id="user-data">
                         <p>Follows: {this.state.user.follows_count}</p>
                         <p>Followers: {this.state.user.followers_count}</p>
                         <p>Posts: {this.state.user.media_count}</p>
                     </div>
                 </div>
+                <h1>Analysis</h1>
                 <GoogleCV media={this.state.user.media} />
             </div>
         );
