2ed9783c9ad0be4bc2fbbda0ab97dc12ea59fe28
Andrew Tat <andrewtat@me.com>
2019-05-13 20:43:38 -0600

    Filter out images from Instagram API request response

--- src/User.js
+++ src/User.js
@@ -13,22 +13,24 @@ export default class User extends Component {
     }
 
     render() {
+        // Filter out any non-images
+        const userMedia = this.props.user.media.filter((media) => media.media_type === "IMAGE");
         return (
             <div id="user">
                 <div id="bio">
                     <div id="profile-picture">
-                        <img src={this.state.user.profile_picture_url} alt={this.state.user.name} width="400" height="400" />
+                        <img src={this.props.user.profile_picture_url} alt={this.props.user.name} width="400" height="400" />
                     </div>
-                    <p>{this.state.user.name}</p>
-                    <p>@{this.state.user.username}</p>
+                    <p>{this.props.user.name}</p>
+                    <p>@{this.props.user.username}</p>
                     <div id="user-data">
-                        <p>Follows: {this.state.user.follows_count}</p>
-                        <p>Followers: {this.state.user.followers_count}</p>
-                        <p>Posts: {this.state.user.media_count}</p>
+                        <p>Follows: {this.props.user.follows_count}</p>
+                        <p>Followers: {this.props.user.followers_count}</p>
+                        <p>Posts: {this.props.user.media_count}</p>
                     </div>
                 </div>
                 <h1>Analysis</h1>
-                <GoogleCV media={this.state.user.media} />
+                <GoogleCV media={userMedia} />
             </div>
         );
     }
